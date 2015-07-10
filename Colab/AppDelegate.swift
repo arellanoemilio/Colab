e711159391067d12082de06b4338d6a7a62118ce
asdmikk <k2rnermikk@gmail.com>
2015-07-10 17:17:12 +0200

    added parse frameworks n stuff

--- Colab/AppDelegate.swift
+++ Colab/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Parse
+import Bolts
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
 		// Override point for customization after application launch.
+		
+		Parse.enableLocalDatastore()
+		
+		// Initialize Parse.
+		Parse.setApplicationId("QSkDUFTM4Li9PDQUCDdf7asD9sOdq8tNHyj5PAfh",
+			clientKey: "MnO4v4SUY5eGSVphWAnhLgtUTDjBcw4GbOSqxAnm")
+		
+		// [Optional] Track statistics around application opens.
+		PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+		
 		return true
 	}
 
