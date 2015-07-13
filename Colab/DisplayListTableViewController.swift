9cb78c6d9f08febd767f579f95d35f35672d0e3a
Emilio Arellano <arellanoemilio@gmail.com>
2015-07-13 10:02:43 +0200

    added the DisplayListTableViewController

---
+++ Colab/DisplayListTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DisplayListTableViewController.swift
+//  Colab
+//
+//  Created by Emilio Arellano on 7/13/15.
+//  Copyright (c) 2015 mikkkarner. All rights reserved.
+//
+
+import Cocoa
+
+class DisplayListTableViewController: UITableViewController {
+    
+}
