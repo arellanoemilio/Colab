d5b063a8744b9c1ac8012b3cdd2925fec3c50b04
asdmikk <k2rnermikk@gmail.com>
2015-07-13 14:19:19 +0200

    stuff

---
+++ Colab/SetupPageContentViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SetupPageContentViewController.swift
+//  Colab
+//
+//  Created by Mikk Kärner on 13/07/15.
+//  Copyright (c) 2015 mikkkarner. All rights reserved.
+//
+
+import UIKit
+
+class SetupPageContentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
