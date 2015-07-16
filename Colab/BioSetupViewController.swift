53fa1860e0c04ff77934a86f4a6055f57c3e34e9
asdmikk <k2rnermikk@gmail.com>
2015-07-16 10:39:24 +0200

    added email

--- Colab/BioSetupViewController.swift
+++ Colab/BioSetupViewController.swift
@@ -12,6 +12,7 @@ import Parse
 class BioSetupViewController: UIViewController {
 
 	@IBOutlet weak var bioField: UITextView!
+	@IBOutlet weak var emailField: UITextField!
 	
 	var user: PFUser!
 	
@@ -33,21 +34,31 @@ class BioSetupViewController: UIViewController {
 	
 	@IBAction func done(sender: UIButton) {
 		var bio = bioField.text
+		var email = emailField.text
+		if count(email) < 1 {
+			let alertController = UIAlertController(title: "Dude!", message:
+				"Enter your contact email!", preferredStyle: UIAlertControllerStyle.Alert)
+			alertController.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default,handler: nil))
+			return
+		}
+		if count(bio) < 1 {
+			let alertController = UIAlertController(title: "Dude!", message:
+				"Add something to your bio!", preferredStyle: UIAlertControllerStyle.Alert)
+			alertController.addAction(UIAlertAction(title: "Fine", style: UIAlertActionStyle.Default,handler: nil))
+			return
+		}
 		if count(bio) > 255 {
 			let alertController = UIAlertController(title: "Dude!", message:
 				"Your bio is to long!", preferredStyle: UIAlertControllerStyle.Alert)
 			alertController.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default,handler: nil))
 			return
 		}
-		if count(bio) > 0 {
-			user["bio"] = bio
-			user.saveInBackground()
-			performSegueWithIdentifier("toHome", sender: sender)
-		} else {
-			let alertController = UIAlertController(title: "Dude!", message:
-				"Add something to your bio!", preferredStyle: UIAlertControllerStyle.Alert)
-			alertController.addAction(UIAlertAction(title: "Fine", style: UIAlertActionStyle.Default,handler: nil))
-		}
+		
+		user["bio"] = bio
+		user.email = email
+		user.saveInBackground()
+		performSegueWithIdentifier("toHome", sender: sender)
+		
 	}
 	
 	override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
