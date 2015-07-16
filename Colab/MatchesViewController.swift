7faf967183823f549f157eca5b757a994364ad4a
Emilio Arellano <arellanoemilio@gmail.com>
2015-07-16 11:27:35 +0200

    fixed querry

--- Colab/MatchesViewController.swift
+++ Colab/MatchesViewController.swift
@@ -32,8 +32,6 @@ class MatchesViewController: UIViewController {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
-        query()
-		
 		userImageView.layer.cornerRadius = 20
 		userImageView.layer.masksToBounds = true
 		
@@ -47,6 +45,8 @@ class MatchesViewController: UIViewController {
 	
 	override func viewWillAppear(animated: Bool) {
 		super.viewWillAppear(animated)
+        
+        query()
 	}
 	
 	@IBAction func like(sender: AnyObject) {
